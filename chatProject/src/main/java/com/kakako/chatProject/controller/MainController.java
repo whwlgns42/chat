a3cd323ae79bc752d96f20c09bf85a0821e7b6f8
조지훈 <whwlgns42@kakao.com>
2024-06-29 17:27:02 +0900

    init

---
+++ chatProject/src/main/java/com/kakako/chatProject/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.kakako.chatProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main() {
+		System.out.println("메인");
+		return "elements";
+	}
+}
